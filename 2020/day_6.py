79b39235c8c9c84d7ed30166a2fe49e10fb5dbcb
Russell, Kim <KRussell@crowdcompass.com>
2020-12-06 11:02:50 -0800

    cleaning up day_6 and updating bingo card

--- 2020/day_6.py
+++ 2020/day_6.py
@@ -1,31 +1,45 @@
+#day 6
 import string
 
 groups = []
-count_total = 0
-alphabet =  list(string.ascii_lowercase)
 
 with open("day_6_input.txt", "r") as data:
     input = data.read()
-    groups = input.replace('\n', ' ')
-print(groups)
+
+groups = input.replace('\n', ' ')
 groups = list(groups.split('  '))
-print(groups)
 
-for group in groups:
-    user_answers = group.split(' ')
-    for char in alphabet:
+# part_1
+def answered_once_per_group(groups):
+    count_total = 0
+    for group in groups:
+        answers = list(group.replace(' ', ""))
+        no_dups = []
         count = 0
-        for answer in user_answers:
-            if char in answer:
-                count = count + 1
-        if count == len(user_answers):
-            count_total = count_total + 1
-    print("user_answers " + str(user_answers))
-    print("---")
-print(count_total)
-    
+        #print(answers)
+        for answer in answers:
+            if answer not in no_dups: 
+                no_dups.append(answer)
+        count = len(no_dups)
+        count_total = count_total + count
+    return count_total
 
+# part_2
+def answered_all_in_group(groups):
+    count_total = 0
+    alphabet =  list(string.ascii_lowercase)
+    for group in groups:
+        user_answers = group.split(' ')
+        for char in alphabet:
+            count = 0
+            for answer in user_answers:
+                if char in answer:
+                    count = count + 1
+            if count == len(user_answers):
+                count_total = count_total + 1
+        #print("user_answers " + str(user_answers))
+        #print("---")
+    return(count_total)
 
-    
-
-
+print("part_1: " + str(answered_once_per_group(groups))) 
+print("part_2: " + str(answered_all_in_group(groups)))
