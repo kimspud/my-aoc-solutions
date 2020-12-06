d39decfa6aaca4c0dc6b90304b59ec05931c8fc8
Russell, Kim <KRussell@crowdcompass.com>
2020-12-06 10:33:25 -0800

    part 2

--- 2020/day_6.py
+++ 2020/day_6.py
@@ -2,23 +2,26 @@ import string
 
 groups = []
 count_total = 0
+alphabet =  list(string.ascii_lowercase)
 
 with open("day_6_input.txt", "r") as data:
     input = data.read()
     groups = input.replace('\n', ' ')
-
+print(groups)
 groups = list(groups.split('  '))
+print(groups)
 
 for group in groups:
-    answers = list(group.replace(' ', ""))
-    no_dups = []
-    count = 0
-    #print(answers)
-    for answer in answers:
-        if answer not in no_dups: 
-            no_dups.append(answer)
-    count = len(no_dups)
-    count_total = count_total + count
+    user_answers = group.split(' ')
+    for char in alphabet:
+        count = 0
+        for answer in user_answers:
+            if char in answer:
+                count = count + 1
+        if count == len(user_answers):
+            count_total = count_total + 1
+    print("user_answers " + str(user_answers))
+    print("---")
 print(count_total)
     
 
