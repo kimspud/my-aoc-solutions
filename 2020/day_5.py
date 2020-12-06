2cd395fa3f61c2f104f858a6c1c192ea7335d819
Russell, Kim <KRussell@crowdcompass.com>
2020-12-06 01:35:30 -0800

    day 5

---
+++ 2020/day_5.py
@@ -0,0 +1,59 @@
+#day_5
+row_code = []
+column_code = []
+seat_codes = []
+max_row = 127
+min_row = 0
+max_column = 7
+min_column = 0
+seat_ids = []
+
+def find_row(row_code, max_row, min_row):
+    print("row found " + str(row_code))
+    for r in row_code:
+        mid = (min_row + max_row) // 2
+        if r == "F": 
+            max_row = mid   
+        elif r == "B":
+            min_row = mid  + 1
+        else:
+            print("else")
+    if max_row != min_row:
+        print("something is wrong")
+    return(max_row)
+    
+def find_column(column_code, max_column, min_column):
+    print("column found " + str(column_code))
+    for c in column_code:
+        mid = (min_column + max_column) // 2
+        if c == "L": 
+            max_column = mid    
+        elif c == "R":
+            min_column = mid + 1
+        else:
+            print("else")
+    if max_column != min_column:
+        print("something is wrong")
+    return(max_column)
+
+
+with open("day_5_input.txt", "r") as data:
+    for line in data:
+        seat_codes.append(line.strip())
+    #passports = input.replace('\n', ' ')
+    print(seat_codes)
+
+for code in seat_codes:
+    seat_code = code
+    row_code = []
+    column_code = []
+    for char in seat_code:
+        if char == "B" or char == "F":
+            row_code.append(char)
+        else:
+            column_code.append(char)
+    row = find_row(row_code, max_row, min_row)
+    column = find_column(column_code, max_column, min_column)
+    seat_id =  row * 8 + column
+    seat_ids.append(seat_id)
+print(max(seat_ids))
