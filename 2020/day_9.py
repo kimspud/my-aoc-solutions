433e91672e540d72308aa126f33a8dc79cc6de01
Russell, Kim <KRussell@crowdcompass.com>
2020-12-09 21:04:43 -0800

    day 9 part 2

--- 2020/day_9.py
+++ 2020/day_9.py
@@ -12,10 +12,9 @@ def number_not_a_sum(numbers, start, stop):
     while stop < len(numbers):
         range_list = numbers[start:stop]
         sums = []
-        print(f"range_list: {range_list}")
-        #starting at index 5 + 1, is the current number in the string of possible sums
-        #find if 
+        #print(f"range_list: {range_list}")
         length = len(range_list)
+
         for i in range(length):
             for v in range_list:
                 #print(f"v: {v}")
@@ -25,9 +24,37 @@ def number_not_a_sum(numbers, start, stop):
         #print(f"sums: {sums}")
         if int(numbers[stop]) not in sums:
             print(f"{int(numbers[stop])} is not a sum of the other numbers.")
-            break
+            return int(numbers[stop])
         stop = stop + 1
         start = start + 1
+
+
+def part_2(numbers, invalid_number):
+    #print(f"numbers + {numbers}")
+    
+    temp = []   # just gets rid of a warning
+
+    for i in range(0, len(numbers)):
+        temp = []
+        j = i
+        sum = 0
+        while sum < invalid_number:
+            value = int(numbers[j])
+            sum = sum + value
+            # print("value: " + str(value) + " sum: " + str(sum))
+            temp.append(value)
+            j = j + 1
+        
+        # print(f"sum is {sum}")
+        if (sum == invalid_number):
+            break
+
+    result = max(temp) + min(temp)
+    print("result: " + str(result))
+
         
 # part 1
 number_not_a_sum(numbers, 0, 25)
+
+# part 2
+part_2(numbers, 756008079)
